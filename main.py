1462142d34e22d23c284b07cf4a089907bfb62b0
arpcorrea <andre.rittner.correa@gmail.com>
2021-06-09 18:14:33 -0400

    added unified data excel

--- main.py
+++ main.py
@@ -6,17 +6,38 @@ Created on Wed Jun  9 10:29:15 2021
 """
 from PronyModulus import *
 
-import pandas
+import pandas as pd
 from scipy import optimize
 from scipy.optimize import curve_fit
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-df=pandas.read_excel('data180.xlsx')
+
+
+
+xl = pd.ExcelFile('data.xlsx')
+data_dict={}
+for name in xl.sheet_names:
+    df = xl.parse(name)
+    data_dict[name]=df
+    
+    
+
+
+
+
+df = pandas.read_excel('data.xlsx', '180' )
+df2 = pandas.read_excel('data.xlsx', '160' )
 frequency = df.values[:,0]
 storage = df.values[:,1]
 loss = df.values[:,2]
+
+# for sheet_name 
+
+# data ={}
+# for name in sheet_name:
+#     data.update[name, value]
 
 
 # points per decade
